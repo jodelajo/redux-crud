65821100a6ffadc122bf1235c811a97068b58ef7
jodelajo <jodelajo@gmail.com>
2022-03-27 14:21:16 +0200

    Add user

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { addUser } from "./features/users";
 
 function App() {
+  const dispatch = useDispatch();
+  const userList = useSelector((state) => state.users.value);
+  console.log("userlist", userList);
+  const [newUser, setNewUser] = useState({
+    id: 0,
+    name: "",
+    username: "",
+  });
+
+  const onChangeHandlerName = (e) => {
+    e.preventDefault();
+    setNewUser({ ...newUser, name: e.target.value, id: userList.length + 1 });
+  };
+
+  const onChangeHandlerUsername = (e) => {
+    e.preventDefault();
+    setNewUser({ ...newUser, username: e.target.value });
+  };
+
+  console.log("new user", newUser);
+  console.log(userList.length);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="addUser">
+        <input
+          name="name"
+          type="text"
+          placeholder="Name..."
+          onChange={onChangeHandlerName}
+        />
+        <input
+          type="text"
+          placeholder="Username..."
+          name="username"
+          onChange={onChangeHandlerUsername}
+        />
+        <button onClick={() => dispatch(addUser(newUser))}>Add User</button>
+      </div>
+      <div className="displayUsers">
+        {userList.map((user) => {
+          return (
+            <div key={user.id}>
+              <h1>{user.name}</h1>
+              <h2>{user.username}</h2>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
